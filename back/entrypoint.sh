97219cd9bfe0496d0c0e13d456fcba279a88a88e
zero <zero@0x.local>
2025-11-11 12:59:49 +0100

    Arreglos en entrypoint del backend y mapeo de puertos a laravel

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cp .env.example .env
+
 echo "Esperando a que MySQL esté disponible..."
 until nc -z db 3306; do
   echo "Aún no está lista la base de datos..."
@@ -7,8 +9,8 @@ until nc -z db 3306; do
 done
 
 echo "Base de datos lista, ejecutando migraciones..."
-php artisan migrate --force
+php artisan migrate --force 
 
-echo "Iniciando PHP-FPM..."
-exec php-fpm
+echo "Iniciando servidor de laravel puerto 8000"
+exec php artisan serve --host=0.0.0.0 --port=8000
 
